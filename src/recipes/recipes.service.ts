20439768cc8f57c49d78c878048e4517f73be4ff
Christine Garibian <christinegaribian@gmail.com>
2023-12-18 22:57:51 +0400

    recipes fixups

--- src/recipes/recipes.service.ts
+++ src/recipes/recipes.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { CosmosDbService } from '../db/cosmos-db.service';
-import { RecipeDto } from './dto/recipe.dto'; // Assuming you have a DTO for recipes
+import { RecipeDto } from './dto/recipe.dto';
+
+const RECIPE_CONTAINER_NAME = 'Recipes';
 
 @Injectable()
 export class RecipesService {
@@ -9,24 +11,24 @@ export class RecipesService {
     async create(recipeDto: RecipeDto): Promise<any> {
         // Assuming 'recipes' is the container name
         console.log("in the recipe service. dto:    " + recipeDto);
-        return this.cosmosDbService.createItem('Recipes', recipeDto);
+        return this.cosmosDbService.createItem(RECIPE_CONTAINER_NAME, recipeDto);
     }
 
     async findOne(id: string): Promise<any> {
-        return this.cosmosDbService.getItemById('Recipes', id);
+        return this.cosmosDbService.getItemById(RECIPE_CONTAINER_NAME, id);
     }
 
     async findAll(): Promise<any[]> {
-        return this.cosmosDbService.getAllItems('Recipes');
+        return this.cosmosDbService.getAllItems(RECIPE_CONTAINER_NAME);
     }
 
     async update(id: string, recipeDto: RecipeDto): Promise<any> {
         // Ensure the id is included in the update data
         const updatedRecipe = { id, ...recipeDto };
-        return this.cosmosDbService.updateItem('Recipes', id, updatedRecipe);
+        return this.cosmosDbService.updateItem(RECIPE_CONTAINER_NAME, id, updatedRecipe);
     }
 
     async delete(id: string): Promise<void> {
-        return this.cosmosDbService.deleteItem('Recipes', id);
+        return this.cosmosDbService.deleteItem(RECIPE_CONTAINER_NAME, id);
     }
 }
