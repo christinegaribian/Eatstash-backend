28b41b3c0341445ceaf63364ce1c56787d4c7495
Christine Garibian <christinegaribian@gmail.com>
2023-12-18 22:08:10 +0400

    create generic CRUD ops for db

--- src/db/cosmos-db.service.ts
+++ src/db/cosmos-db.service.ts
@@ -1,54 +1,44 @@
 import { Injectable } from '@nestjs/common';
 import { CosmosClient, Database, Container } from '@azure/cosmos';
-const partitionKey = { kind: 'Hash', paths: ['/partitionKey'] }
+// const partitionKey = { kind: 'Hash', paths: ['/partitionKey'] }
 
 @Injectable()
 export class CosmosDbService {
     private client: CosmosClient;
     private database: Database;
-    private container: Container;
 
     constructor() {
-        console.log('CosmosDB Endpoint:', process.env.COSMOSDB_ENDPOINT);
-        console.log('CosmosDB Key:', process.env.COSMOSDB_KEY);
         this.client = new CosmosClient({
             endpoint: process.env.COSMOSDB_ENDPOINT,
             key: process.env.COSMOSDB_KEY
         });
-
-        this.initialize();
+        this.initializeDatabase();
     }
 
-    private async initialize() {
-        // Connect to database
+    private async initializeDatabase() {
         const dbResponse = await this.client.databases.createIfNotExists({
             id: 'ToDoList'
         });
         console.log(`Created database:\nToDoList\n`)
-
         this.database = dbResponse.database;
+    }
 
-        // Connect to container
-        const containerResponse = await this.database.containers.createIfNotExists({
-            id: 'Items'
-        });
-        console.log(`Created container:\nItems\n`)
-
-        this.container = containerResponse.container;
+    private getContainer(containerName: string): Container {
+        return this.database.container(containerName);
     }
 
     // CRUD operations go here
     // Create
-    async createItem(item: any): Promise<any> {
-        const { resource } = await this.container.items.create(item);
-        return resource;
+    async createItem(containerName: string, item: any): Promise<any> {
+        const container = this.getContainer(containerName);
+        return (await container.items.create(item)).resource;
     }
 
-    // Read
-    async getItem(id: string): Promise<any> {
+    // Read by ID
+    async getItemById(containerName: string, id: string): Promise<any> {
+        const container = this.getContainer(containerName);
         try {
-            const { resource } = await this.container.item(id).read();
-            return resource;
+            return (await container.item(id).read()).resource;
         } catch (error) {
             // Handle not found error or other errors
             throw error;
@@ -56,8 +46,9 @@ export class CosmosDbService {
     }
 
     // Read All
-    async getAllItems(): Promise<any[]> {
-        const { resources } = await this.container.items
+    async getAllItems(containerName: string): Promise<any[]> {
+        const container = this.getContainer(containerName);
+        const { resources } = await container.items
             .query({
                 query: 'SELECT * FROM c'
             })
@@ -66,10 +57,10 @@ export class CosmosDbService {
     }
 
     // Update
-    async updateItem(id: string, newItem: any): Promise<any> {
+    async updateItem(containerName: string, id: string, updatedItem: any): Promise<any> {
+        const container = this.getContainer(containerName);
         try {
-            const { resource } = await this.container.item(id).replace(newItem);
-            return resource;
+            return (await container.item(id).replace(updatedItem)).resource;
         } catch (error) {
             // Handle not found error or other errors
             throw error;
@@ -77,9 +68,10 @@ export class CosmosDbService {
     }
 
     // Delete
-    async deleteItem(id: string): Promise<void> {
+    async deleteItem(containerName: string, id: string): Promise<void> {
+        const container = this.getContainer(containerName);
         try {
-            await this.container.item(id).delete();
+            await container.item(id).delete();
         } catch (error) {
             // Handle not found error or other errors
             throw error;
