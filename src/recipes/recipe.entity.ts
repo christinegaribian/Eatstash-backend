bb7df363757f76c84f82340b84687b92176086d8
Christine Garibian <christinegaribian@gmail.com>
2023-12-18 23:23:45 +0400

    add recipe entity

---
+++ src/recipes/recipe.entity.ts
@@ -0,0 +1,7 @@
+export class Recipe {
+    id: string; // Unique identifier for the recipe
+    title: string; // Title of the recipe
+    ingredients: string[]; // List of ingredients
+    instructions: string; // Cooking instructions
+    // Other relevant fields, like preparation time, servings, etc.
+}
